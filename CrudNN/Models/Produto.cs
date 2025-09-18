c44bd98c5b35f1cef82248db741ba022fbf4e802
Adryan-Francisco <adryanfrancisco62@gmail.com>
2025-09-18 16:29:54 -0300

    Update 18-09

---
+++ CrudNN/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CrudNN.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudNN.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Required, StringLength(120)]
+        public string Nome { get; set; } = string.Empty;
+        [ModelBinder(BinderType = typeof(InvariantDecimalModelBinder))]
+        [Range(0, 999999999)]
+        public decimal Preco { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Estoque { get; set; }
+        public ICollection<VendaProduto>? Vendas { get; set; }
+
+    }
+}
---
+++ CrudNN/ViewModels/VendaFormVM.cs
@@ -0,0 +1,9 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace CrudNN.ViewModels;
+public class VendaFormVM
+{
+    [Required]
+    public int ClienteId { get; set; }
+    public List<VendaItemVM> Itens { get; set; } = new();
+}
